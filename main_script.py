started working on parallel code, fixed NoneType reducing computation speed

main_script.py
@@ -3,6 +3,7 @@ from questrade_api import Questrade
 import my_fun
 import time
 import os
+import parallel_fun
 
 ### --- Initialization --- ###
 q = Questrade()
@@ -16,8 +17,6 @@ IEX_token = 'pk_8ae91e6a0b11401b88e3d2b71aae39a7'
 num_days_a_year = 252
 fixed_commission = 9.95
 contract_commission = 1
-call_sell_max = 10
-put_sell_max = 10
 list_len = 100
 best_returns = np.zeros((list_len, 9))
 
@@ -39,30 +38,35 @@ adjusted_current_price = my_fun.adjust_prices(
     expiry_dates_new, naked_current_price, naked_history, IEX_token, stock_of_interest, last_div_index)
 
 # Should be: range(0,len(all_options_data))
-for n in range(0, 1):
+for n in range(0, len(all_options_data)):
     strike_date_index = n
     strike_date = expiry_dates_new[n]
     current_price_at_exp = adjusted_current_price.get(strike_date)
     # COULD ADD EXTRA 1 FOR THE WEEKEND! Or if early in the day / market hasn't opened
     days_till_expiry = np.busday_count(current_date, strike_date)
-    # print(days_till_expiry)
     # Taking the percent change and applying to the current price
     hist_final_price = my_fun.historical_final_price(
         naked_history, current_price_at_exp, days_till_expiry)
+    print(time.time() - t)
     # extracting options data
     daily_option_data = all_options_data[n]['chainPerRoot'][0]['chainPerStrikePrice']
     # sorted prices of options based on cost, call price and put price (in that order)
     sorted_prices = my_fun.price_sorting_v2(
         daily_option_data, strike_date, stock_of_interest)
+    print(time.time() - t)
     # Simulation based off our data
     # TODO: Currently we don't take into account the number of call/put contracts actually available
     # we are assuming that it is less than the call and put max we set
     [percent_chance_in_money, historical_return_avg, risk_money] = \
         my_fun.risk_analysis_v3(sorted_prices, current_price_at_exp, fixed_commission,
                                 contract_commission, hist_final_price, call_sell_max=2, put_sell_max=2)
+    # parallel_fun.risk_analysis_v4(sorted_prices, current_price_at_exp, fixed_commission,
+    #                               contract_commission, hist_final_price)
+    # my_fun.risk_analysis_v3(sorted_prices, current_price_at_exp, fixed_commission,
+    #                         contract_commission, hist_final_price, call_sell_max=2, put_sell_max=2)
+    print(time.time() - t)
     best_returns = my_fun.find_best(best_returns, percent_chance_in_money, historical_return_avg,
                                     sorted_prices, strike_date_index, days_till_expiry)
-    print(time.time() - t)
 
 
 my_results = my_fun.beautify_to_df(best_returns, expiry_dates_new)

my_fun.py
@@ -6,7 +6,7 @@ from questrade_api import Questrade
 import urllib
 import math
 import os
-from numba import njit, prange
+from numba import prange, jit
 
 q = Questrade()
 
@@ -83,7 +83,7 @@ def extract_price_history_v2(stock_of_interest, API_key):
 # Scale historical prices to remove role played by dividends
 
 
-@njit(parallel=True)
+@jit(parallel=True, nopython=True)
 def get_naked_prices(my_history_price, current_price, num_days_a_year):
     naked_history = my_history_price.copy()
     adjust_matrix = np.zeros((len(my_history_price), 1))
@@ -157,6 +157,7 @@ def adjust_prices(expiry_dates_new, naked_current_price, naked_history, IEX_toke
 # This function calculates the theoretical end prices of the stock at the expiry date
 
 
+@jit(parallel=True, nopython=True)
 def historical_final_price(naked_price_history, current_price, days_till_expiry):
     final_prices = np.zeros(
         (int(len(naked_price_history) - days_till_expiry), 1))
@@ -196,11 +197,19 @@ def price_sorting_v2(option_data, strike_date, stock_name):
             optionIds=list(id_holder))['optionQuotes']
     for n in range(0, len(option_data)):
         bid_call_price = call_put_data[2 * n]['bidPrice']
+        if bid_call_price == None:
+            bid_call_price = 0
         ask_call_price = call_put_data[2 * n]['askPrice']
+        if ask_call_price == None:
+            ask_call_price = 0
         bid_call_size = call_put_data[2 * n]['bidSize']
         ask_call_size = call_put_data[2 * n]['askSize']
         bid_put_price = call_put_data[2 * n + 1]['bidPrice']
+        if bid_put_price == None:
+            bid_put_price = 0
         ask_put_price = call_put_data[2 * n + 1]['askPrice']
+        if ask_put_price == None:
+            ask_put_price = 0
         bid_put_size = call_put_data[2 * n + 1]['bidSize']
         ask_put_size = call_put_data[2 * n + 1]['askSize']
         price_holder[n, 1:] = [bid_call_price, bid_call_size, ask_call_price, ask_call_size,
@@ -212,6 +221,7 @@ def price_sorting_v2(option_data, strike_date, stock_name):
     for n in range(0, len(price_holder)):
         if price_holder[n, 2] == 0:
             data_down = data_down + 1
+    # If data is down, we try to load it locally
     if data_down == len(price_holder):
         print('Questrade data is down! Trying to pull most recent data...')
         try:
@@ -221,13 +231,14 @@ def price_sorting_v2(option_data, strike_date, stock_name):
         except:
             print('Most recent data not found!')
             pass
+    # If data is not down, then we save it locally
     if data_down < len(price_holder):
-        col_names = ['Strike Price', 'Call bid price', 'Call bid size', 'Call ask price', 'Call ask size', \
-        'Put bid price', 'Put bid size', 'Put ask price', 'Put ask size']
+        col_names = ['Strike Price', 'Call bid price', 'Call bid size', 'Call ask price', 'Call ask size',
+                     'Put bid price', 'Put bid size', 'Put ask price', 'Put ask size']
         if (os.path.exists('bid_history/' + stock_name) == False):
             os.makedirs('bid_history/' + stock_name)
-        pd.DataFrame(columns = col_names, data = price_holder).to_csv('bid_history/' + stock_name + '/' +
-                                          str(strike_date) + '.csv', encoding='utf-8', index=True)
+        pd.DataFrame(columns=col_names, data=price_holder).to_csv('bid_history/' + stock_name + '/' +
+                                                                  str(strike_date) + '.csv', encoding='utf-8', index=True)
         print('Questrade data saved locally!')
     return price_holder
 
@@ -237,7 +248,6 @@ def price_sorting_v2(option_data, strike_date, stock_name):
 # Calculates the max increase or decrease in stock price while remaining in safe zone.
 # call price is on rows, put price is on columns
 # first sheet is max increase, second sheet is max decrease
-
 
 def risk_analysis_v3(sorted_prices, current_price, fixed_commission, contract_commission, final_prices,
                      call_sell_max=1, put_sell_max=1):
@@ -249,16 +259,12 @@ def risk_analysis_v3(sorted_prices, current_price, fixed_commission, contract_co
         (len(sorted_prices), len(sorted_prices)), dtype=np.ndarray)
     # The rows represent call prices
     for n in range(0, len(sorted_prices)):
-        # Progress indicator
-        print(n)
         call_strike_price = sorted_prices[n, 0]
         call_premium = sorted_prices[n, 1]
         call_size = sorted_prices[n, 2]
         # The columns represent put prices
         for m in range(0, len(sorted_prices)):
-            # reinitilaizing the max call and put matrix
-            num_call_put_matrix = np.zeros((call_sell_max, put_sell_max))
-            # reinitilaizing other matrices
+            # reinitilaizing inner matrices
             historical_return_avg_inner = np.zeros(
                 (call_sell_max + 1, put_sell_max + 1))
             percent_in_money_inner = np.zeros(
@@ -269,20 +275,21 @@ def risk_analysis_v3(sorted_prices, current_price, fixed_commission, contract_co
             put_premium = sorted_prices[m, 5]
             put_size = sorted_prices[m, 6]
             ###
-            # Seeing if these options actually exist
-            if (call_premium == None) or (put_premium == None):
-                percent_in_money[n, m] = None
-                historical_return_avg[n, m] = None
+            # Seeing if these options actually exist, if not then just assign everything 0
+            if (call_premium == 0) or (put_premium == 0):
+                percent_in_money[n, m] = 0
+                historical_return_avg[n, m] = 0
+                risk_money[n, m] = 0
             else:
                 # Calls
                 call_base = np.minimum(
                     call_strike_price - final_prices, 0) + call_premium
+                # Creates the matrix of all possible call_sell amounts
                 call_num_matrix = np.arange(
                     0, call_sell_max + 1, 1).reshape(1, call_sell_max + 1)
                 call_comm_matrix = fixed_commission + call_num_matrix * contract_commission
                 call_comm_matrix[0][0] = 0
                 call_return = call_base * call_num_matrix * 100 - call_comm_matrix
-
                 # Puts
                 put_base = np.minimum(
                     final_prices - put_strike_price, 0) + put_premium
@@ -345,10 +352,10 @@ def find_best(best_returns, percent_in_money, historical_return_avg, sorted_pric
                 historical_return_avg[n, m] * 0.01 * (1 / days_till_expiry)
             # Method below does not take into account the percent chance of being in money, only avg return / day
             # daily_info = historical_return_avg[n, m] * (1 / days_till_expiry)
+            if isinstance(daily_info, np.float64):
+                continue
             daily_returns = np.append(np.ndarray.flatten(
                 daily_info), np.array(best_returns[:, 0]))
-            daily_returns = np.array(
-                [x for x in daily_returns if str(x) != 'nan'])
             new_best = sorted((daily_returns[np.argpartition(daily_returns, (-list_len))][(-list_len):]),
                               reverse=True)
             # See if there has been any changes to the best returns matrix

sandbox.py
@@ -4,6 +4,8 @@ from questrade_api import Questrade
 import my_fun
 import time
 import pandas as pd
+import copy
+from numba import njit, prange, jit
 
 ### --- Initialization --- ###
 q = Questrade()
@@ -41,30 +43,73 @@ exp_dates_adjusted_current_price = my_fun.adjust_prices(
 strike_date = expiry_dates_new[0]
 print(strike_date)
 
-my_list = ['Strike Date', 'Sold Date', 'Type', 'Strike Price', 'Sold Price', 'Sold Quantity']
+my_list = ['Strike Date', 'Sold Date', 'Type',
+           'Strike Price', 'Sold Price', 'Sold Quantity']
 
 my_list_2 = ['Transaction Date', 'Strike Date', 'Buy / Sell', 'Type', 'Strike Price', 'Premium Price',
              'Quantity', 'Change in Cash (USD)']
 
 df = pd.DataFrame({'year': [2019],
-                    'month': [11],
-                    'day': [29]})
+                   'month': [11],
+                   'day': [29]})
 my_dates = pd.to_datetime(df, format='%Y%m%d')
 date_1 = my_dates.iloc[0].asm8.astype('<M8[D]')
 
 purchase_time = pd.to_datetime('2019-11-20 12:49:00')
 purchase_time
 
-my_data = np.array([[date_1, purchase_time, 'Call', 129.0, 6.4, 3],\
+my_data = np.array([[date_1, purchase_time, 'Call', 129.0, 6.4, 3],
                     [date_1, purchase_time, 'Put', 140.0, 5.4, 3]])
 
 my_data_2 = np.array([[purchase_time, date_1, 'Sell', 'Call', 129.0, 6.4, 3, 1907],
                       [purchase_time, date_1, 'Sell', 'Put', 140.0, 5.4, 3, 1607]])
 
-live_purchases = pd.DataFrame(columns = my_list, data = my_data)
+live_purchases = pd.DataFrame(columns=my_list, data=my_data)
 
-transactions = pd.DataFrame(columns = my_list_2, data = my_data_2)
+transactions = pd.DataFrame(columns=my_list_2, data=my_data_2)
 
 live_purchases.to_csv('live_options.csv', encoding='utf-8', index=True)
 
 transactions.to_csv('transaction_history.csv', encoding='utf-8', index=True)
+
+
+@jit(parallel=True, nopython=False)
+def risk_analysis_v4(call_sell_max=2, put_sell_max=2):
+
+    sorted_prices = np.zeros((3, 1))
+
+    for n in range(call_sell_max + 1):
+        globals()['hist_return_avg_' + str(n) + str(put_sell_max)] = \
+            np.zeros((len(sorted_prices), len(sorted_prices)))
+        globals()['percent_in_money' + str(n) + str(put_sell_max)] = \
+            np.zeros((len(sorted_prices), len(sorted_prices)))
+        globals()['risk_money' + str(n) + str(put_sell_max)] = \
+            np.zeros((len(sorted_prices), len(sorted_prices)))
+    for n in range(put_sell_max):
+        globals()['hist_return_avg_' + str(call_sell_max) + str(n)] = \
+            np.zeros((len(sorted_prices), len(sorted_prices)))
+        globals()['percent_in_money' + str(call_sell_max) + str(n)] = \
+            np.zeros((len(sorted_prices), len(sorted_prices)))
+        globals()['risk_money' + str(call_sell_max) + str(n)] = \
+            np.zeros((len(sorted_prices), len(sorted_prices)))
+
+    return(hist_return_avg_21)
+
+a = 5
+b = c = d = copy.deepcopy(a)
+b += 4
+
+qwe = np.ones((5,1)) * 2
+asd = np.ones((1,2)) *3
+np.multiply(qwe, asd)
+
+
+@njit(parallel=True)
+def fuck_numba():
+    qwe = np.ones((5,1)) * 2
+    asd = np.ones((1,2)) * 3
+    fff = qwe * asd
+    return fff
+
+
+print(fuck_numba())